4b6cfcf342d8f0b563818c30be310a832f09ecd5
Josue025012 <josuenascimento572@gmail.com>
2026-03-12 17:17:54 -0300

    sistema feito e estilo básico aplicado

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,58 @@
-import { Text, View } from "react-native";
+import React, {useState} from "react";
+import { Text, View, Button, TouchableOpacity } from "react-native";
+
 
 export default function Index() {
+  const [n, setN] = useState(0)
+
+  function sortearNumero() {
+    setN(Math.floor(Math.random() * 100) + 1);
+  }
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 24,
+        backgroundColor: "#333",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text 
+      style={{
+        width: 280,
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: "regular",
+        color: "white",
+      }}>
+        O número sorteado foi: {n}
+      </Text>
+      <TouchableOpacity
+      style={{
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width: 280,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      onPressIn={sortearNumero}
+      activeOpacity={0.8}>
+        <Text
+        style={{
+          width: 100,
+          textAlign: "center",
+          color: "black",
+          fontSize: 16,
+          fontWeight: "bold",
+          boxShadow: "solid",
+          shadowColor: "black",
+        }}>
+          Sortear
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
